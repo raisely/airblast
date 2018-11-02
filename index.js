ef2b3287f69f1977204762e26e3ba5f6269c2a60
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2018-11-02 21:15:04 +0800

    Add init helper to main index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const AirblastController = require('./src/controller');
 const routes = require('./src/routes');
+const init = require('./src/init');
 
 module.exports = {
 	AirblastController,
 	routes,
+	init,
 };
