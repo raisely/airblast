279168f615712bb5ff542b069e3859a51567fc59
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2018-11-03 22:52:00 +0800

    Document cronengine

--- sample/cronengine/index.js
+++ sample/cronengine/index.js
@@ -1,3 +1,8 @@
+/**
+  * Google Cloud Functions does not provide any kind of cron, so
+  * this is a simple app engine that will run retry's for the
+  * controllers
+  */
 const cronEngine = require('airblast/cronEngine');
 const crontab = require('./crontab');
 
