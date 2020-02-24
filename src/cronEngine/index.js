ee61f1b542ecaaa6964354bb3ee9706244b36940
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-24 11:56:27 +0800

    Set user agent for cron requests

--- src/cronEngine/index.js
+++ src/cronEngine/index.js
@@ -1,10 +1,17 @@
 const { CronJob } = require('cron');
 const request = require('request-promise-native');
+const package = require('../../package');
 
 let cronTasks = [];
 
 function makeRequest(req) {
-	request(req).catch(console.error);
+	const options = typeof req === 'string' ? { uri: req } : { ...req };
+	Object.assign(options, {
+		headers: {
+			'User-Agent': `Airblast Cron Engine ${package.version}`,
+		},
+	});
+	request(options).catch(console.error);
 }
 
 module.exports = {
